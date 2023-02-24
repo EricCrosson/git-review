8fb6cebc1d6b3bc159dd67042b7fa8b97d6f4da0
Eric Crosson <eric.s.crosson@utexas.edu>
2023-02-24 07:35:36 -0600

    feat: add help flag
    Closes #1

--- git-review.sh
+++ git-review.sh
@@ -1,11 +1,69 @@
 #!/usr/bin/env bash
+#
+# Review and merge PRs from your terminal
+#
+# Usage:
+#  git-review <pr-number>
+#
+# Options:
+#   <pr-number>    GitHub pull request number
+#
+# @example
+# git review 1234
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-pull_request="${1:?Must specify pull request on which to operate}"
+usage() {
+  cat <<EOF
 
-gh pr diff "$pull_request"
-gh pr review "$pull_request"
-gh pr merge --merge --delete-branch "$pull_request"
+Usage:
+ git-review <pr-number>
+
+Options:
+  <pr-number>    GitHub pull request number
+EOF
+}
+
+# Parse arguments
+pull_request=""
+
+while [ "${1:-}" != "" ]; do
+  case "$1" in
+  -h | --help)
+    usage
+    exit 1
+    ;;
+    # Matches every non-integer argument
+  *[!0-9]*)
+    usage
+    exit 1
+    ;;
+  *)
+    pull_request="$1"
+    shift # past value
+    ;;
+  esac
+done
+
+# Validate arguments
+validation_errors=false
+
+if [ -z "$pull_request" ]; then
+  echo >&2 "Error: must supply <pr-number>"
+  validation_errors=true
+fi
+
+if [ "$validation_errors" == true ]; then
+  usage
+  exit 1
+fi
+
+main() {
+  gh pr diff "$pull_request"
+  gh pr review "$pull_request"
+  gh pr merge --merge --delete-branch "$pull_request"
+}
+
+main
